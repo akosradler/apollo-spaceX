149f6fee77c8411761ce565252bd2c527dda5469
Akos Radler <akos.radler@gmail.com>
2020-11-19 19:10:38 +0100

    Building the schema

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,11 @@
-require('dotenv').config();
+const { ApolloServer } = require('apollo-server')
+const typeDefs = require('./schema')
+
+const server = new ApolloServer({ typeDefs })
+
+server.listen().then(() => {
+  console.log(`
+    Server is running!
+    Listening on port 4000
+  `)
+})
